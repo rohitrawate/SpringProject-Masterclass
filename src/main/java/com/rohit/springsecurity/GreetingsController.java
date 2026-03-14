9d53b6dd9982bbae44cf0a155659f9cd24b52027
rohitrawate <rohitrawate007@gmail.com>
2026-03-14 17:26:11 +0530

    Basic Authentication Stateless with Spring Security hardcoded username and pass in Properties

---
+++ src/main/java/com/rohit/springsecurity/GreetingsController.java
@@ -0,0 +1,14 @@
+package com.rohit.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello";
+    }
+
+}
---
+++ src/main/java/com/rohit/springsecurity/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.rohit.springsecurity;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws  Exception {
+        http.authorizeHttpRequests( authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated());
+        http.sessionManagement( session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    
+}
