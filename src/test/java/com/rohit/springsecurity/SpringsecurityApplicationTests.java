1baf45a516cdd648e325e672c2bfe4d0fb0323d7
rohitrawate <rohitrawate007@gmail.com>
2026-03-14 17:01:18 +0530

    Project Structure

---
+++ src/test/java/com/rohit/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
