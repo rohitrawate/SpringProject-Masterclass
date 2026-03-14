56fe97eb29519ef6a3ef59d5552d9a21244ae225
rohitrawate <rohitrawate007@gmail.com>
2026-03-15 00:06:40 +0530

    In memory Authentication

--- src/main/java/com/rohit/springsecurity/SecurityConfig.java
+++ src/main/java/com/rohit/springsecurity/SecurityConfig.java
@@ -10,8 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,5 +32,19 @@ public class SecurityConfig {
         return http.build();
     }
 
-    
+    @Bean
+    public UserDetailsService  userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}pass")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
+    }
+
 }
